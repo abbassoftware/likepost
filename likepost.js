1a7c4ee7e192719084942ee6507a8000ce70bc7d
Abbas <abbas.s786@yahoo.co.in>
2015-03-06 08:41:13 +0530

    Made changes based on review comments

--- likepost.js
+++ likepost.js
@@ -7,8 +7,12 @@ jQuery(document).ready(function () {
 			url: this.href,
 			dataType: 'json',
 			success: function (data) {
-				if(data.like_status == 0)	jQuery('a.like-link').html('Like');
-				else jQuery('a.like-link').html('Delete Like');
+				if(data.like_status == 0) {
+					jQuery('a.like-link').html('Like');
+				}
+				else {
+					jQuery('a.like-link').html('Delete Like');
+				}
 
 				jQuery('.total_count').html(data.total_count);
 			},
